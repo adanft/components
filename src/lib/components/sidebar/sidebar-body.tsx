8f94da7e6943c69d47bde1d9a756617afd252d5d
adanft <franco.start.moba@gmail.com>
2026-03-23 07:05:59 -0400

    fix(sidebar-body): replace 96px magic number with 6rem

--- src/lib/components/sidebar/sidebar-body.tsx
+++ src/lib/components/sidebar/sidebar-body.tsx
@@ -1,15 +1,18 @@
-import type { ComponentPropsWithoutRef, ReactNode } from 'react';
+import type { ComponentPropsWithoutRef, ReactNode } from "react";
 
-import SimpleBar from 'simplebar-react';
-import 'simplebar-react/dist/simplebar.min.css';
-import { cn } from '../../helpers/cn';
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
+import { cn } from "../../helpers/cn";
 
-type SidebarBodyProps = ComponentPropsWithoutRef<'div'> & {
+type SidebarBodyProps = ComponentPropsWithoutRef<"div"> & {
   children: ReactNode;
 };
 
 function SidebarBody({ children, className }: SidebarBodyProps) {
-  const componentClass = cn('max-h-[calc(100vh-96px)] overflow-x-hidden', className);
+  const componentClass = cn(
+    "max-h-[calc(100vh-6rem)] overflow-x-hidden",
+    className,
+  );
   return (
     <SimpleBar className={componentClass}>
       <nav className="flex flex-col gap-2">{children}</nav>
