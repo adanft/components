d3bc54b719ddffd913d05595e0bfb78b87c8cae5
adanft <franco.start.moba@gmail.com>
2026-05-04 08:55:03 -0400

    style(ui): refine semantic colors

--- packages/ui/src/components/breadcrumbs/page.tsx
+++ packages/ui/src/components/breadcrumbs/page.tsx
@@ -5,7 +5,7 @@ import { cn } from '../../helpers/cn';
 type BreadcrumbsPageProps = ComponentPropsWithoutRef<'span'>;
 
 function BreadcrumbsPage({ className, ...props }: BreadcrumbsPageProps) {
-  return <span {...props} aria-current="page" className={cn('font-medium text-foreground', className)} />;
+  return <span {...props} aria-current="page" className={cn('font-medium text-heading', className)} />;
 }
 
 export default BreadcrumbsPage;
